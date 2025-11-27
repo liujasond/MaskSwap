716dac9cc06343ec7b4eaa36a510f28e931c59cd
liujasond <znybguy60043@outlook.com>
2025-11-27 12:57:51 +0800

    deploy fix

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -5,13 +5,20 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  const deployedFHECounter = await deploy("FHECounter", {
+  const cusdtDeployment = await deploy("ConfidentialUSDT", {
     from: deployer,
     log: true,
   });
 
-  console.log(`FHECounter contract: `, deployedFHECounter.address);
+  const maskSwapDeployment = await deploy("MaskSwap", {
+    from: deployer,
+    args: [cusdtDeployment.address],
+    log: true,
+  });
+
+  console.log(`ConfidentialUSDT contract: ${cusdtDeployment.address}`);
+  console.log(`MaskSwap contract: ${maskSwapDeployment.address}`);
 };
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_maskswap"; // id required to prevent reexecution
+func.tags = ["MaskSwap"];
